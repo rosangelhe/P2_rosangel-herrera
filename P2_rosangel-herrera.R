907e0837e955343d032dcaa52f13e7e1c4b84762
Rosangel Herrera <rosangelh350@gmail.com>
2022-11-18 11:54:40 -0400

    initial commit

---
+++ P2_rosangel-herrera.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(ggthemes)
+
+#Carga de la base de datos en formato .CSV
+international_matches <- read_csv("World+Cup/international_matches.csv")
+X2022_world_cup_groups <- read_csv("World+Cup/2022_world_cup_groups.csv")
+world_cup_matches <- read_csv("World+Cup/world_cup_matches.csv")
