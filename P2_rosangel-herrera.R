93e3efac55e9b63dcac02cdaa2cd43cf420b1192
Rosangel Herrera <rosangelh350@gmail.com>
2022-11-22 18:28:07 -0400

    Resultados historicos de juegos en la historia de los 32 equipos participantes en Qatar 2022

--- P2_rosangel-herrera.R
+++ P2_rosangel-herrera.R
@@ -54,12 +54,88 @@ top_fav <- total_top_5 %>%
 
 #ranking FIFA de como inician al mundial las 32 selecciones
 
-r_fifa22 <- X2022_world_cup_groups[c("FIFA Ranking", "Team")]
-
 r_fifa22 <- X2022_world_cup_groups
 r_fifa22 <- r_fifa22[with(r_fifa22, order(r_fifa22$`Group`)), ]
 
 #-----------------------------------------------------------------------------#
+
+#filtro cantidad de partidos ganados por seleccion e instancias que llegaron
+match_w_columns <- c("Team", "Wins")
+win_f22 <- world_cup_matches
+
+home_win <- win_f22 %>% 
+  group_by(`Home Team`) %>%
+  summarise(`Home Goals` = sum(`Home Goals` > `Away Goals`), )
+colnames(home_win) <- match_w_columns
+
+away_win <- win_f22 %>% 
+  group_by(`Away Team`) %>%
+  summarise(`Away Goals` = sum(`Away Goals` > `Home Goals`), )
+colnames(away_win) <- match_w_columns
+
+total_matchs <- merge(home_win, away_win, by = "Team", all = TRUE)
+total_matchs$Wins <- total_matchs$Wins.x + total_matchs$Wins.y
+total_matchs <- total_matchs[with(total_matchs, order(-Wins)), ]
+
+#filtro cantidad de partidos empatados por seleccion e instancias que llegaron
+match_t_columns <- c("Team", "Ties")
+
+tie_match <- win_f22 %>% 
+  group_by(`Home Team`) %>%
+  summarise(`Home Goals` = sum(`Home Goals` == `Away Goals`), )
+colnames(tie_match) <- c("Team", "Ties")
+
+tie_match1 <- win_f22 %>% 
+  group_by(`Away Team`) %>%
+  summarise(`Away Goals` = sum(`Away Goals` == `Home Goals`), )
+colnames(tie_match1) <- c("Team", "Ties")
+
+total_matchs1 <- merge(tie_match, tie_match1, by = "Team", all = TRUE)
+total_matchs1$Ties <- total_matchs1$Ties.x + total_matchs1$Ties.y
+total_matchs1 <- total_matchs1[with(total_matchs1, order(-Ties)), ]
+
+
+#filtro cantidad de partidos perdidos por seleccion e instancias que llegaron
+match_l_columns <- c("Team", "Loss")
+
+home_loss <- win_f22 %>% 
+  group_by(`Home Team`) %>%
+  summarise(`Home Goals` = sum(`Home Goals` < `Away Goals`), )
+colnames(home_loss) <- match_l_columns
+
+away_loss <- win_f22 %>% 
+  group_by(`Away Team`) %>%
+  summarise(`Away Goals` = sum(`Away Goals` < `Home Goals`), )
+colnames(away_loss) <- match_l_columns
+
+total_matchs2 <- merge(home_loss, away_loss, by = "Team", all = TRUE)
+total_matchs2$Loss <- total_matchs2$Loss.x + total_matchs2$Loss.y
+total_matchs2 <- total_matchs2[with(total_matchs2, order(-Loss)), ]
+
+#Combinacion de tablas
+all_matchs <- left_join(total_matchs, total_matchs1, by = "Team") %>%
+  left_join(total_matchs2, by = "Team")
+
+#Resultados de historicos de juegos en la historia del FIFA World Cup
+all_WC <- select(all_matchs, Team, Wins, Ties, Loss)
+
+#Resultados de historicos de juegos en la historia de los 32 equipos participantes 
+#en Qatar 2022
+
+q_2022 <- all_WC %>% 
+  filter(`Team` %in% c("Qatar", "Germany", "Denmark", "Brazil", "France",
+                       "Belgium", "Croatia", "Spain", "Serbia", "England", "Switzerland", "Netherlands", 
+                       "Argentina", "Iran", "South Korea", "Japan", "Saudi Arabia", "Ecuador",
+                       "Uruguay", "Canada","Ghana", "Senegal", "Portugal", "Poland", "Tunisia", 
+                       "Morocco", "Cameroon","Mexico", "United States", "Wales", "Australia", 
+                       "Costa Rica")) %>%
+  select(Team, Wins, Ties, Loss)
+
+#Anexion de la seleccion de Qatar dado a su primer torneo es en este torneo
+
+q_2022 <- q_2022 %>%
+  rows_upsert(data.frame(Team = "Qatar", Wins = 0, Ties = 0, Loss = 0))
+
 
 #----------- Parte II Visualizacion de los datos "Data Cleaning" -------------#
 
